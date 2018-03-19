676e80a2a7f166e91038ed70c4d01b6dbb519e1a
Sun-Jung Lin <everjoey@gmail.com>
2018-03-19 08:07:51 +0000

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+import random
+import time
 
+def bubble_sort(a):
+	i = len(a) - 1
+	flag = 1
+	while flag == 1:
+		flag = 0
+		for j in range(i):
+			if a[j] > a[j+1]:
+				a[j], a[j+1] = a[j+1], a[j]
+				flag = 1
+		i -= 1
