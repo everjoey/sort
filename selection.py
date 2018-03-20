83d7e91b4fb40f512082fd558cd5a2bd3ea392d1
Sun-Jung Lin <everjoey@gmail.com>
2018-03-20 05:36:21 +0000

    Update selection.py

--- selection.py
+++ selection.py
@@ -3,9 +3,9 @@ import random
 import cProfile
 
 def selection(a):
-	for i in range(0, len(a) - 1):		
+	for i in range(0, len(a) - 1):
 		min_val_index = i
-		for j in range(i + 1, len(a)):						
+		for j in range(i + 1, len(a)):
 			if a[j] < a[min_val_index]:
 				min_val_index = j
 		a[i], a[min_val_index] = a[min_val_index], a[i]
@@ -13,5 +13,5 @@ def selection(a):
 if __name__ == '__main__':
 	a = [random.randrange(1000) for i in range(1000)]
 	print(a)
-	cProfile.run('bubble(a)')
+	cProfile.run('selection(a)')
 	print(a)
