69135e845b7ebb98b46764fa91792a7a9134458a
Sun-Jung Lin <everjoey@gmail.com>
2018-03-20 05:24:07 +0000

    Update selection.py

--- selection.py
+++ selection.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+import random
+import cProfile
 
+if __name__ == '__main__':
+	a = [random.randrange(1000) for i in range(1000)]
+	print(a)
+	cProfile.run('bubble(a)')
+	print(a)
