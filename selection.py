ac5af1935000f19c1e3f525f3c192e7514c7d75a
Sun-Jung Lin <everjoey@gmail.com>
2018-03-23 06:13:41 +0000

    Update selection.py

--- selection.py
+++ selection.py
@@ -10,6 +10,8 @@ def selection(a):
 				min_val_index = j
 		a[i], a[min_val_index] = a[min_val_index], a[i]
 
+def selection(a): for i in range(0, len(a) - 1):  min_val_index = i  for j in range(i + 1, len(a)):   if a[j] < a[min_val_index]:    min_val_index = j  a[i], a[min_val_index] = a[min_val_index], a[i]
+		
 if __name__ == '__main__':
 	a = [random.randrange(1000) for i in range(1000)]
 	print(a)
